480c0180892d07f2087c51dc6a4f7213fc6587f5
anuj840 <ssrivastava123123456@gmail.com>
2022-02-25 23:30:58 -0500

    Elements Folder added

---
+++ ElementsSelector/twitterselector.js
@@ -0,0 +1,20 @@
+const root = document.getElementsByClassName("css-1dbjc4n r-18u37iz r-13qz1uu r-417010")[0];
+
+
+
+const leftMenu = root.childNodes[2]
+const middleandRight = root.childNodes[3]
+const whatshapping = document.getElementsByClassName("css-1dbjc4n r-aqfbo4 r-zso239 r-1hycxz")[0];
+
+
+const leftside=leftMenu.getElementsByClassName("css-1dbjc4n r-1habvwh r-eqz5dr")[0];
+const leftsideHome = leftside.childNodes[0]
+const leftsideExplore = leftside.childNodes[1]
+const leftsideNotification = leftside.childNodes[2]
+const leftsideMessages = leftside.childNodes[3]
+const leftsideProfile = leftside.childNodes[4]
+const  leftsideMore =  leftside.childNodes[5]
+
+
+const leftsidemore = document.getElementsByClassName("css-1dbjc4n r-1awozwy r-18u37iz");
+
